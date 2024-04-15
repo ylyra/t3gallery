18272c2268c2eadffc9b0298eb59f0661b4b7ec2
Yan Lyra <yanlyra3@gmail.com>
2024-04-14 22:50:03 -0300

    chore: not skipe ts and lint error if locally

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from 'next/constants.js';
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -5,7 +7,7 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {
+const baseConfig = {
   images: {
     remotePatterns: [
       {
@@ -13,13 +15,32 @@ const config = {
       }
     ]
   },
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
 };
 
+/**
+ * @param {string} phase
+ * @returns {Promise<import("next").NextConfig>} 
+ * @see https://nextjs.org/docs/api-reference/next.config.js/introduction
+ * @see https://nextjs.org/docs/api-reference/next.config.js/environment-variables
+ */
+const config = async (phase) => {
+  console.log(!process.env.CI && phase === PHASE_PRODUCTION_BUILD, phase);
+  if (
+    phase === PHASE_DEVELOPMENT_SERVER ||
+    !process.env.CI && phase === PHASE_PRODUCTION_BUILD
+  ) {
+    return baseConfig
+  }
+
+  return {
+    ...baseConfig,
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    }
+  };  
+  
+}
 export default config;
